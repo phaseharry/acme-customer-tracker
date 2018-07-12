78e8f398d84d49949e5cf86d4bba0d061b4d5757
Harry C <harrychenhc@icloud.com>
2018-07-11 23:14:37 -0400

    refactored

--- customers.js
+++ customers.js
@@ -1,16 +1,36 @@
 function generateCustomerSalesMap(salesData, customerData) {
   return customerData.reduce(function(obj, customer) {
-    let total = 0;
-    for (let i = 0; i < salesData.length; i++) {
-      if (salesData[i].customerID === customer.ID) {
-        total += salesData[i].total;
+    let total = salesData.reduce(function(accum, sale) {
+      if (sale.customerID === customer.ID) {
+        return accum + sale.total;
+      } else {
+        return accum;
       }
-    }
+    }, 0);
     let name = customer.name;
     obj[name] = total;
     return obj;
   }, {});
 }
+/*refactored to use a reduce within the first reduce to loop through all sales
+data and increment the sales total with ID's matching the current customer
+*/
+
+/* function generateCustomerSalesMap(salesData, customerData) {
+    return customerData.reduce(function(obj, customer) {
+      let total = 0;
+      for (let i = 0; i < salesData.length; i++) {
+        if (salesData[i].customerID === customer.ID) {
+          total += salesData[i].total;
+        }
+      }
+      let name = customer.name;
+      obj[name] = total;
+      return obj;
+    }, {});
+  }
+*/
+
 /*
 Line 2: The initial reduce function loops through the customer data.
 Line 4 - 8: Loops through the sales data looking for IDs on sales that match the current customer's ID.
